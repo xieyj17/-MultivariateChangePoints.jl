8bf80f1eb398a46efb848b06ff9f992854aa8d38
xieyj17 <yijun.xie@uwaterloo.ca>
2022-02-17 17:47:56 -0500

    bug with alpha for disco distance

--- src/cp.jl
+++ src/cp.jl
@@ -1,4 +1,4 @@
-function DiscoDistance(x::Vector{Float64}, y::Vector{Float64}; pd::Int64=1)::Float64
+function DiscoDistance(x::Vector{Float64}, y::Vector{Float64}; α::Real=1)::Float64
     n = length(x)
     m = length(y)
 
@@ -6,21 +6,21 @@ function DiscoDistance(x::Vector{Float64}, y::Vector{Float64}; pd::Int64=1)::Flo
 
     for i in 1:n
         for j in 1:m
-            p1 += (abs(x[i] - y[j]))^pd
+            p1 += (abs(x[i] - y[j]))^α
         end
     end
     p1 = p1*2/(m*n)
     
     for i in 1:(n-1)
         for k in (i+1):n
-            p2 += (abs(x[i] - x[k]))^pd
+            p2 += (abs(x[i] - x[k]))^α
         end
     end
     p2 = p2*2/(n*n)
 
     for j in 1:(m-1)
         for l in (j+1):m
-            p3 += (abs(y[j]-y[l]))^pd
+            p3 += (abs(y[j]-y[l]))^α
         end
     end
     p3 = p3*2/(m*m)
